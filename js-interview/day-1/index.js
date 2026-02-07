693cb324628ca6c2f173c7dbf2958549cc000aa4
pintusingh123 <pintugit982@gmail.com>
2026-02-07 12:48:35 +0530

    created js-ligic-buil folder

---
+++ js-interview/day-1/index.js
@@ -0,0 +1,53 @@
+// function abc(a, b) {
+//   if (b > a) {
+//     console.log("b is large");
+//   } else {
+//     console.log("a is large");
+//   }
+//   return a + b;
+// }
+
+// console.log(abc(4, 1));
+// console.log(abc(1, 4));
+
+// /.................................
+
+// function abc(a,...b , c ){
+//   return[...a,...b,c]
+// }
+// console.log(abc([1,2],3,4));
+
+// ...............................
+
+// const name ="harikrishna"
+// console.log(name());
+
+// .....................
+
+// function abc() {
+//   return 100 + 200;
+// }
+// var abc;
+// console.log(abc());
+
+// /...................
+
+//  console.log(typeof typeof 100);
+// ...........
+ 
+// const abc = [..."hari"];
+// console.log(abc)
+
+// /.......................
+
+ 
+ 
+// console.log(parseInt("A100"));
+// console.log(parseInt("100A"));
+
+//...................................
+console.log([100,200,300].map(ele=>{
+  if(ele > 90) return ;
+  return ele*10;
+}));
+
---
+++ js-logic-building/basics/oddEven.js
@@ -0,0 +1,18 @@
+// odd even 
+// case 1 
+// function oddEven(num){
+//     if(num % 2 === 0){
+//         return "even";
+//     }
+//     return "odd";
+// }
+// console.log(oddEven(23));
+
+// case 2
+// function oddEven(num){
+//     if(num & 1){
+//         return "odd";
+//     }
+//     return "even";
+// }
+// console.log(oddEven(21));
